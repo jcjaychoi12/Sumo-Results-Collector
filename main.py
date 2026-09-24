1bd5715f8a374e8ccba5c140d33717ae7dcebfc7
Jungbin Choi <jungbin.l.choi@gmail.com>
2026-09-24 10:30:16 -0400

    Setup selenium webdriver

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+
 def main() -> None:
-    pass
+    url: str = "https://www.sumo.or.jp/ResultBanzuke/table/"
+    driver: WebDriver = webdriver.Chrome()
+
+    try:
+        driver.get(url)
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        print(soup.prettify())
+    except Exception as e:
+        print(f"[Error] Failed to access {url}\n\n{e}")
+    finally:
+        driver.quit()
 
 
 if __name__ == "__main__":
