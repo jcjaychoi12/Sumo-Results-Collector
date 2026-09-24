20ea0d295fe5b0127432b6602985fb272627ebf1
Jungbin Choi <jungbin.l.choi@gmail.com>
2026-09-24 10:54:47 -0400

    Enable headless webdriver
    Enable headless option for the webdriver. Also added messages for creating source.html

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 def main() -> None:
     url: str = "https://www.sumo.or.jp/ResultBanzuke/table/"
-    driver: WebDriver = webdriver.Chrome()
+    chrome_options: Options = Options()
+    chrome_options.add_argument("--headless")
+    driver: WebDriver = webdriver.Chrome(options=chrome_options)
 
     try:
         driver.get(url)
-        soup = BeautifulSoup(driver.page_source, "html.parser")
+        soup: BeautifulSoup = BeautifulSoup(driver.page_source, "html.parser")
         create_html_file(soup.prettify())
     except Exception as e:
         print(f"[Error] Failed to access {url}\n\n{e}")
     finally:
         driver.quit()
 
+    return
+
 
 def create_html_file(html: str) -> None:
-    with open("source.html", mode="w", encoding="utf-8") as file:
-        file.write(html)
+    try:
+        with open("source.html", mode="w", encoding="utf-8") as file:
+            file.write(html)
+        print("[Success] Created source.html")
+    except Exception as e:
+        print(f"[Error] Failed to create source.html file\n\n{e}")
     return
 
 
